0c6a4650511983dcb7ff4360b1747cd900b4a758
unknown <nexloder05@gmail.com>
2024-01-28 11:28:06 +0200

    feat: add redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import './index.css';
+import App from './App.jsx';
+import store from './redux/store/store.js';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
     <App />
+  </Provider>
 );
